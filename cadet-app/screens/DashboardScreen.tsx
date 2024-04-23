cc2cc361dee0534ff300ad23a103153954869a37
NojoMac <123109830+NojoMac@users.noreply.github.com>
2024-04-23 11:31:39 -0400

    Maps Works

--- cadet-app/screens/DashboardScreen.tsx
+++ cadet-app/screens/DashboardScreen.tsx
@@ -27,6 +27,7 @@ const DashboardScreen = ({ navigation }) => {
     longitude: 0,
     altitude: 0,
   });
+  
   const PlanOfTheDay = `
   RESEARCH AND SYMPOSIUM DAY
 
@@ -46,11 +47,6 @@ const DashboardScreen = ({ navigation }) => {
   2300: Taps
   `;
 
-  const trainingsAt1100 = `  1/c - Trainings could be listed here
-  2/c - With locations included (Dimick)
-  3/c - Or Pressable links to online Trainings (Click Me)
-  4/c - Updated Conduct System Brief (Leamy)`;
-
   const commonPeople = `  Command:
   Medical:
   Facilities:
@@ -68,35 +64,6 @@ const DashboardScreen = ({ navigation }) => {
   (987) 314-1592
   (656) 271-8281
   `;
-
-  const users = [
-    {
-      name: "brynn",
-      avatar: "https://uifaces.co/our-content/donated/1H_7AxP0.jpg",
-    },
-    {
-      name: "thot leader",
-      avatar:
-        "https://images.pexels.com/photos/598745/pexels-photo-598745.jpeg?crop=faces&fit=crop&h=200&w=200&auto=compress&cs=tinysrgb",
-    },
-    {
-      name: "jsa",
-      avatar: "https://uifaces.co/our-content/donated/bUkmHPKs.jpg",
-    },
-    {
-      name: "talhaconcepts",
-      avatar: "https://randomuser.me/api/portraits/men/4.jpg",
-    },
-    {
-      name: "andy vitale",
-      avatar: "https://uifaces.co/our-content/donated/NY9hnAbp.jpg",
-    },
-    {
-      name: "katy friedson",
-      avatar:
-        "https://images-na.ssl-images-amazon.com/images/M/MV5BMTgxMTc1MTYzM15BMl5BanBnXkFtZTgwNzI5NjMwOTE@._V1_UY256_CR16,0,172,256_AL_.jpg",
-    },
-  ];
 
   type CardsComponentsProps = {};
 
